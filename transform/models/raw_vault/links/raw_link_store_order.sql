47f2af0e7916e1486748d9223d0a4a46e79a0981
Mattias Thalén <bitter-polders0x@icloud.com>
2024-03-28 20:23:56 +0000

    Lint jinja

--- transform/models/raw_vault/links/raw_link_store_order.sql
+++ transform/models/raw_vault/links/raw_link_store_order.sql
@@ -2,10 +2,10 @@
 
 {%- set source_model = ["v_stg_orders"] -%}
 
-{%- set src_pk = "store_order_hk"         -%}
+{%- set src_pk = "store_order_hk" -%}
 {%- set src_fk = ["store_hk", "order_hk"] -%}
-{%- set src_ldts = "load_date"            -%}
-{%- set src_source = "source"             -%}
+{%- set src_ldts = "load_date" -%}
+{%- set src_source = "source" -%}
 
 {{ automate_dv.link(src_pk=src_pk, src_fk=src_fk, src_ldts=src_ldts,
                     src_source=src_source, source_model=source_model) }}
